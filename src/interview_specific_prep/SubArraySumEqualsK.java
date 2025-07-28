c605d84ae8cb3ce21de7da72498d941aab5e9fdf
Ronak Sengupta <senguptaronak48@gmail.com>
2025-07-28 23:14:56 +0530

    Adding solution for subarray sum equals k

---
+++ src/interview_specific_prep/SubArraySumEqualsK.java
@@ -0,0 +1,28 @@
+package interview_specific_prep;
+
+import java.util.Arrays;
+import java.util.HashMap;
+
+public class SubArraySumEqualsK {
+    public static void main(String[] args){
+        int[] nums = {1,2,3};
+        int k=3;
+        System.out.println(subArraySum(nums, k));
+
+    }
+
+    private static int subArraySum(int[] nums, int k){
+        int count = 0;
+        int currentSum = 0;
+        HashMap<Integer, Integer> map = new HashMap<>();
+        map.put(0,1);
+        for(int num : nums){
+            currentSum += num;
+            if(map.containsKey(currentSum-k)){
+                count += map.get(currentSum-k);
+            }
+            map.put(currentSum, map.getOrDefault(currentSum, 0) + 1);
+        }
+        return count;
+    }
+}
