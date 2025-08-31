1bc955207c80f6dbf13717950fe657f68fa1b9e0
Ronak Sengupta <senguptaronak48@gmail.com>
2025-08-31 18:01:30 +0530

    Adding soln for stairs climibing problem

---
+++ src/interview_specific_prep/ClimbingStairs.java
@@ -0,0 +1,19 @@
+package interview_specific_prep;
+
+public class ClimbingStairs {
+
+    public static void main(String[] args) {
+        int n = 5;
+        System.out.println(new ClimbingStairs().climbStairs(n));
+    }
+    public int climbStairs(int n) {
+        if(n <= 2) return n;
+        int[] dp = new int[n+1];
+        dp[1] = 1;
+        dp[2] = 2;
+        for(int i=3; i<=n; i++){
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        return dp[n];
+    }
+}
